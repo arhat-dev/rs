bf321a14297f2cc897f8c47a70e734a4833811d1
Jeffrey Stoke <me@arhat.dev>
2021-10-11 12:03:55 +0200

    test(patch): Add patch spec yaml tests
    Fix plain scalar value resolving
    
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- patch_test.go
+++ patch_test.go
@@ -1,6 +1,10 @@
 package rs
 
 import (
+	"bytes"
+	"io/fs"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -224,4 +228,44 @@ func TestPatchSpec_ApplyTo(t *testing.T) {
 			)
 		})
 	}
+}
+
+func TestPatchSpec(t *testing.T) {
+	filepath.WalkDir("testdata/patch-spec", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		specYaml, err := os.ReadFile(path)
+		if !assert.NoError(t, err, "failed to read test spec %q", d.Name()) {
+			return err
+		}
+
+		t.Run(d.Name(), func(t *testing.T) {
+			dec := yaml.NewDecoder(bytes.NewReader(specYaml))
+
+			var (
+				testSrc  AnyObject
+				expected interface{}
+			)
+
+			assert.NoError(t, dec.Decode(&testSrc), "invalid test source")
+			assert.NoError(t, dec.Decode(&expected), "invalid expected result")
+
+			assert.NoError(t,
+				testSrc.ResolveFields(&testRenderingHandler{}, -1),
+				"failed to resolve test source",
+			)
+
+			ret, err := yaml.Marshal(&testSrc)
+			assert.NoError(t, err, "failed to marshal resolved data")
+
+			var actual interface{}
+			assert.NoError(t, yaml.Unmarshal(ret, &actual), "failed to unmarshal resolved data")
+
+			assert.EqualValues(t, expected, actual)
+		})
+
+		return nil
+	})
 }
