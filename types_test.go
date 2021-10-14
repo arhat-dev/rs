d6f8cf385f96c2bd171cf39b29457a3c3e0dd897
Jeffrey Stoke <me@arhat.dev>
2021-10-14 22:31:20 +0200

    test(type): Add RenderingHandlerFunc test
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- types_test.go
+++ types_test.go
@@ -11,9 +11,23 @@ func TestInterfaceTypeHandleFunc_Create(t *testing.T) {
 	called := false
 	f := InterfaceTypeHandleFunc(func(typ reflect.Type, yamlKey string) (interface{}, error) {
 		called = true
+		assert.EqualValues(t, "test", yamlKey)
 		return nil, nil
 	})
 
-	f.Create(nil, "")
+	f.Create(nil, "test")
+	assert.True(t, called)
+}
+
+func TestRenderingHandler_RenderYaml(t *testing.T) {
+	called := false
+	f := RenderingHandleFunc(func(renderer string, rawData interface{}) (result interface{}, err error) {
+		called = true
+		assert.EqualValues(t, "test", renderer)
+		assert.EqualValues(t, "rawData", rawData)
+		return nil, nil
+	})
+
+	f.RenderYaml("test", "rawData")
 	assert.True(t, called)
 }
