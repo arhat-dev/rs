d1d900d31f03b1d6748e68aba1fcae0e5a413ef2
Jeffrey Stoke <me@arhat.dev>
2021-10-23 12:20:43 +0200

    fix: Wrap rendered data unmarshaling with recover
    related issue: https://github.com/go-yaml/yaml/issues/665
    
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- resolve_test.go
+++ resolve_test.go
@@ -1,10 +1,12 @@
 package rs
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestBaseField_HasUnresolvedField(t *testing.T) {
@@ -13,4 +15,28 @@ func TestBaseField_HasUnresolvedField(t *testing.T) {
 
 	f.addUnresolvedField("test", "test|data", "foo", reflect.Value{}, false, nil)
 	assert.True(t, f.HasUnresolvedField())
+}
+
+// TODO: remove this test once upstream issue solved
+//
+// issue: https://github.com/go-yaml/yaml/issues/665
+func TestResolve_yaml_unmarshal_panic(t *testing.T) {
+	tests := []struct {
+		dataBytes string
+	}{
+		{"#\n- C\nD\n"},
+	}
+
+	for _, test := range tests {
+		var out interface{}
+		func() {
+			defer func() {
+				rec := recover()
+				assert.NotNil(t, rec)
+			}()
+
+			err := yaml.Unmarshal([]byte(test.dataBytes), &out)
+			assert.Error(t, fmt.Errorf("unreachable code: %w", err))
+		}()
+	}
 }
